c92a9837ee27b1d6274585ba9b7beab4b8a35b7d
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-03 22:36:26 +0200

    refactor Galleria

--- src/components/Galleria.jsx
+++ src/components/Galleria.jsx
@@ -1,17 +1,20 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Col, Image, Row, Spinner } from "react-bootstrap";
 
-class Galleria extends Component {
-  state = {
+const Galleria = ({ saga }) => {
+  /*  state = {
     arrayFilms: [],
     isLoading: false,
-  };
+  }; */
 
-  filmsFetch = () => {
+  const [arrayFilms, setArrayFilms] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const filmsFetch = () => {
     //setto lo stato di caricamento a true prima che parta la fetch
-    this.setState({ isLoading: true });
+    setIsLoading(true);
 
-    fetch("http://www.omdbapi.com/?apikey=dab26447&s=" + this.props.saga)
+    fetch("http://www.omdbapi.com/?apikey=dab26447&s=" + saga)
       .then((resp) => {
         if (resp.ok) {
           console.log(resp);
@@ -22,30 +25,28 @@ class Galleria extends Component {
       })
       .then((films) => {
         console.log(films);
-        this.setState({ arrayFilms: films.Search });
-        this.setState({ isLoading: false });
+        setArrayFilms(films.Search);
+        setIsLoading(false);
       })
       .catch((err) => console.log(err));
   };
 
-  componentDidMount() {
-    this.filmsFetch();
-  }
-  render() {
-    return (
-      <section className="my-3 d-flex flex-column">
-        <h2 className="mb-4 d-inline-block">{this.props.saga + " Saga:"}</h2>
-        {this.state.isLoading && <Spinner animation="border" role="status" variant="currentColor"></Spinner>}
-        <Row className="gx-2 gy-2">
-          {this.state.arrayFilms.map((film) => (
-            <Col xs={12} sm={6} md={4} lg={3} xxl={2} key={film.imdbID}>
-              <Image className="img-fluid w-100 poster-film" src={film.Poster} />
-            </Col>
-          ))}
-        </Row>
-      </section>
-    );
-  }
-}
+  useEffect(() => {
+    filmsFetch();
+  }, [saga]);
+  return (
+    <section className="my-3 d-flex flex-column">
+      <h2 className="mb-4 d-inline-block">{saga + " Saga:"}</h2>
+      {isLoading && <Spinner animation="border" role="status" variant="currentColor"></Spinner>}
+      <Row className="gx-2 gy-2">
+        {arrayFilms.map((film) => (
+          <Col xs={12} sm={6} md={4} lg={3} xxl={2} key={film.imdbID}>
+            <Image className="img-fluid w-100 poster-film" src={film.Poster} />
+          </Col>
+        ))}
+      </Row>
+    </section>
+  );
+};
 
 export default Galleria;
