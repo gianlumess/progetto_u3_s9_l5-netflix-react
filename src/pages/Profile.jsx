421c2e33f24e106a7d0bc4361bee1e888ad45b63
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-03 21:54:46 +0200

    aggiunta dark mode a profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,14 +3,14 @@ import ProfileNavbar from "../components/ProfileNavbar";
 import ProfileEditSection from "../components/ProfileEditSection";
 
 const Profile = () => (
-  <>
+  <div data-bs-theme="dark" className="bg-primary text-white">
     <ProfileNavbar />
     <Container>
       <h1 className="display-2">Edit Profile</h1>
       <hr />
       <ProfileEditSection />
     </Container>
-  </>
+  </div>
 );
 
 export default Profile;
